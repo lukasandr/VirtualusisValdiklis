f988b54a6a08b242766f8d92c5eee49e764195e1
Tomas <tom45.germanavicius@gmail.com>
2017-05-23 19:41:02 +0300

    Pridejau networkinga (galbut veikia ant mano kompo)

--- app/src/main/java/com/virtualusisvaldiklis/busitrecias/vitualusisvaldiklis/PagrindinisLangas.java
+++ app/src/main/java/com/virtualusisvaldiklis/busitrecias/vitualusisvaldiklis/PagrindinisLangas.java
@@ -1,10 +1,15 @@
 package com.virtualusisvaldiklis.busitrecias.vitualusisvaldiklis;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.InputType;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,15 +19,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class PagrindinisLangas extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     IsdestymuSarasas isdestymuSarasas;
+    RelativeLayout isdestymuLaukas;
+    PrintWriter outStream;
+    InetAddress serverAddr;
+    Socket socket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (isdestymuSarasas == null) isdestymuSarasas = new IsdestymuSarasas();
         setContentView(R.layout.activity_pagrindinis_langas);
+        isdestymuLaukas = (RelativeLayout)findViewById(R.id.isdestymuLaukas);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -43,6 +64,107 @@ public class PagrindinisLangas extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Bundle b = getIntent().getExtras();
+        boolean loadLayout = false;
+        if(b != null)
+        {
+            loadLayout = b.getBoolean("loadLayout");
+        }
+        if (loadLayout)
+        {
+            int id = b.getInt("id");
+            setupButtons(id);
+            //promptIP(); prasyti ivesti IP, laikinai isjungta
+            startServer(""); // laikina funckija startuoti serveri
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    void setupButtons(int id)
+    {
+        id = 1;
+        Isdestymas isdestymas = IsdestymuSarasas.isdestymai.get(id);
+        for(int i = 0; i < isdestymas.mygtukai.size(); i++)
+        {
+            final Button button = new Button(this);
+            button.setText(isdestymas.mygtukai.get(i).komanda);
+            //button.setId(id);
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            lp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            lp.addRule(RelativeLayout.ALIGN_PARENT_START);
+            lp.setMargins(isdestymas.mygtukai.get(i).pozicija.x,isdestymas.mygtukai.get(i).pozicija.y,0,0);
+            lp.setMarginStart(isdestymas.mygtukai.get(i).pozicija.x);
+            button.setLayoutParams(lp);
+            button.setOnClickListener(new View.OnClickListener() { // clicking the button opens edit prompt
+                @Override
+                public void onClick(View v) {
+                sendToServer(button.getText().toString());
+                }
+            });
+            button.setMinHeight(0);
+            button.setMinWidth(0);
+            isdestymuLaukas.addView(button);
+        }
+    }
+    void sendToServer(final String msg)
+    {
+        new Thread(new Runnable() {
+            public void run() {
+                try
+                {
+                    outStream.println(msg);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    void promptIP()
+    {
+        final String ip[] = new String[1];
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Jugimasis prie serverio");
+        builder.setMessage("Iveskite serverio IP adresa (10.37.35.120)");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                ip[0] = input.getText().toString();
+                startServer(ip[0]);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+    void startServer(String ip)
+    {
+        new Thread(new Runnable(){
+            public void run(){
+                try {
+                    serverAddr = InetAddress.getByName("10.37.35.120");
+                    socket = new Socket(serverAddr, 8998);
+                    outStream = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
     }
 
     @Override
