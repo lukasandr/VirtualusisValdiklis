58b4c5f2ca82f91df0b259c2f2ef085531e234f4
Tomas <tom45.germanavicius@gmail.com>
2017-05-23 21:23:18 +0300

    Pasalinau networkinga, paruosiau darbui.

--- app/src/main/java/com/virtualusisvaldiklis/busitrecias/vitualusisvaldiklis/PagrindinisLangas.java
+++ app/src/main/java/com/virtualusisvaldiklis/busitrecias/vitualusisvaldiklis/PagrindinisLangas.java
@@ -5,11 +5,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,6 +25,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -38,29 +42,17 @@ public class PagrindinisLangas extends AppCompatActivity
     PrintWriter outStream;
     InetAddress serverAddr;
     Socket socket;
+    Boolean isConnected = false;
+    PrintWriter out;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (isdestymuSarasas == null) isdestymuSarasas = new IsdestymuSarasas();
+        isdestymuSarasas = IsdestymuSarasas.getInstance();
         setContentView(R.layout.activity_pagrindinis_langas);
         isdestymuLaukas = (RelativeLayout)findViewById(R.id.isdestymuLaukas);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -75,7 +67,7 @@ public class PagrindinisLangas extends AppCompatActivity
             int id = b.getInt("id");
             setupButtons(id);
             //promptIP(); prasyti ivesti IP, laikinai isjungta
-            startServer(""); // laikina funckija startuoti serveri
+            //startServer(""); // laikina funckija startuoti serveri
         }
     }
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -98,28 +90,18 @@ public class PagrindinisLangas extends AppCompatActivity
             button.setOnClickListener(new View.OnClickListener() { // clicking the button opens edit prompt
                 @Override
                 public void onClick(View v) {
-                sendToServer(button.getText().toString());
+                //sendToServer(button.getText().toString());
+                    try{
+                    out.println(button.getText().toString());}
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
                 }
             });
             button.setMinHeight(0);
             button.setMinWidth(0);
             isdestymuLaukas.addView(button);
         }
-    }
-    void sendToServer(final String msg)
-    {
-        new Thread(new Runnable() {
-            public void run() {
-                try
-                {
-                    outStream.println(msg);
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
     void promptIP()
     {
@@ -146,51 +128,25 @@ public class PagrindinisLangas extends AppCompatActivity
         });
         builder.show();
     }
-    void startServer(String ip)
+
+
+        void startServer(String ip)
     {
-        new Thread(new Runnable(){
-            public void run(){
-                try {
-                    serverAddr = InetAddress.getByName("10.37.35.120");
-                    socket = new Socket(serverAddr, 8998);
-                    outStream = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                }
-                catch(IOException e){
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-
-
     }
 
     @Override
     public void onBackPressed() {
-       // DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-     //   if (drawer.isDrawerOpen(GravityCompat.START)) {
-     //       drawer.closeDrawer(GravityCompat.START);
-//            super.onBackPressed();
-     //   }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.pagrindinis_langas, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -220,16 +176,10 @@ public class PagrindinisLangas extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
-
     public void count(){}
     public void showMainMenu(){}
     public void showReceivedLayouts(){}
     public void showMessage(String message){}
     public void showGallery(){}
     public void loadThumbnails(){}
-
-
-
 }
